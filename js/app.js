3b30bfdb374cce306a06c40b0cf46ad246c12795
Richard Harvey <germoisawesome@gmail.com>
2021-07-16 00:24:30 -0700

    cleaned up a bit of css/html

--- js/app.js
+++ js/app.js
@@ -1,171 +1,173 @@
 "use strict";
 
-const defaultResponse = "Hey! That isn't right! Try again!";
-var score = 0;
+alert("Come join us!!"); //alert for greeting
+var score = 0; //var for scoring
+let userName = prompt("What is your name?");
+console.log(userName);
 
-let guestName = promt("Hello! What's your name?");
-console.log("Guests name is ${guestName}");
-alert("Thanks for coming, ${guestName}");
+alert("Welcome " + userName + "! Have fun with this site!");
 
-// ask question
 //q 1
 function richFirstName() {
-  let richFirstName = prompt("Yes/No: The page creators first name is Rich?");
-  console.log(`Q 1 answer: ${richFirstName}`);
-  switch (richFirstName.toLowerCase()) {
-    case "y":
-    case "yes":
-      var response = "That is correct! Its Rich";
-      alert(response);
-      score++
-      console.log(response);
-      break;
-    case "n":
-    case "no":
-      var response = "That ain't right yo! Its Rich!";
-      alert(response);
-      console.log(response);
-      break;
-
-    default:
-      alert(defaultResponse);
-      console.log(defaultResponse);
-      break;
+  let firstName = prompt("Is my first name Rich?");
+  let name = firstName.toLowerCase();
+  if (name == "y" || name == "yes") {
+    alert("That's correct");
+    score++;
+    return;
+  }
+  if (name == "n" || name == "no") {
+    alert("That is wrong");
+    return;
+  } else {
+    alert("Nope. Try again!");
+    return;
   }
 }
 richFirstName();
 
 //q 2
 function richMilService() {
-  let richMilService = prompt("Yes/No: Is/Was in the US Army?");
-  console.log(`Q 2 answer: ${richMilService}`);
-  switch (richMilService.toLowerCase()) {
-    case "y":
-    case "yes":
-      var response = "Thats right! Rich was a soldier in the army! Its Rich!";
-      alert(response);
-      score++
-      console.log(response);
-      break;
-
-    case "n":
-    case "no":
-      var response = "Nah fam! Try again!";
-      alert(response);
-      console.log(response);
-      break;
+  let milSerivce = prompt("Yes/No: Is/Was in the US Army?");
+  let mil = milSerivce.toLowerCase();
+  if (mil == "y" || mil == "yes") {
+    alert("That is correct!");
+    return;
+  }
+  if (mil == "n" || mil == "no") {
+    alert("That is incorrect");
+    score++;
+    return;
+  } else {
+    alert("Nah fam. Try again");
+    return;
   }
 }
 richMilService();
 
 //q 3
 function richStarWars() {
-  let richStarWars = prompt("Yes/No: Does Rich like/love Star Wars???");
-  console.log(`Q 3 answer: ${richStarWars}`);
-  switch (richStarWars.toLocaleLowerCase()) {
-    case "y":
-    case "yes":
-      var response = "The force is stong with you";
-      alert(response);
-      score++
-      console.log(response);
-      break;
-
-    case "n":
-    case "no":
-      var response = "These aren't the droids you're looking for!";
-      alert(response);
-      console.log(response);
-      break;
+  let starWars = prompt("Do I like Star Wars?");
+  let wars = starWars.toLowerCase();
+  if (wars == "y" || wars == "yes") {
+    alert("That is correct!");
+    score++;
+    return;
+  }
+  if (wars == "n" || wars == "no") {
+    alert("That is incorrect");
+    return;
+  } else {
+    alert("That wont work!");
+    return;
   }
 }
 richStarWars();
 
 //q 4
 function richJob() {
-  let richJob = prompt("Yes/No: Is Rich employed currently?");
-  console.log(`Q 4 answer: ${richJob}`);
-  switch (richJob.toLowerCase()) {
-    case "y":
-    case "yes":
-      var response = "This lazy bum agrees!";
-      alert(response);
-      score++
-      console.log(response);
-      break;
-    case "n":
-    case "no":
-      var response = "No no no ";
-      alert(response);
-      console.log(response);
-      break;
+  let jobs = prompt("Am I currently employed?");
+  let joby = jobs.toLowerCase();
+  if (joby == "y" || joby == "yes") {
+    alert("That is correct!");
+    score++;
+    return;
+  }
+  if (joby == "n" || joby == "no") {
+    alert("That is incorrect");
+    return;
+  } else {
+    alert("Not an acceptable answer!");
+    return;
   }
 }
 richJob();
 
 //q 5
-function richTats(){
-let richTats = prompt("Yes/No: Do I have any tattoos?");
-console.log(`Q 5 answer: ${richTats}`);
-switch (richTats.toLowerCase()) {
-  case "y":
-  case "yes":
-    var response = "You got the power!";
-    alert(response);
-    score++
-    console.log(response);
-    break;
-  case "n":
-  case "no":
-    var response = "But why you wroooong??";
-    alert(response);
-    console.log(response);
-    break;
-}
+function richTats() {
+  let richTats = prompt("Do I have tattoos?");
+  let tats = richTats.toLowerCase();
+  if (tats == "y" || tats == "yes") {
+    alert("Seems to be the correct answer, " + userName + "!");
+    score++;
+    return;
+  }
+  if (tats == "n" || tats == "no") {
+    alert("naahhhh");
+    return;
+  } else {
+    alert("Try that answer again");
+    return;
+  }
 }
 richTats();
 
-//q 6
-function multiGuess(){
+// q 6
+function multiGuess() {
   for (let i = 0; i < 4; i++) {
-    let numb=16
-    let guessNumber = prompt('What\'s my favorite number? Its between 1 and 100.');
+    let numb = 16;
+    let guessNumber = prompt(
+      "What's my favorite number? Its between 1 and 100."
+    );
     if (guessNumber < numb) {
       alert("Wrongo. Too Low!");
-      console.log(guessNumber)
+      console.log(guessNumber);
     }
     if (guessNumber > numb) {
       alert("Uh uh! Too High!");
-      console.log(guessNumber)
+      console.log(guessNumber);
     }
     if (guessNumber == numb) {
       alert("Wow! We have a smart person here!!");
-      score++
-      console.log(guessNumber)
+      score++;
+      console.log(guessNumber);
       return;
     }
   }
-  alert("You didn\'t say the magic word " + userName + "!");
+  alert("You didn't say the magic word " + userName + "!");
 }
-multiGuess()
+multiGuess();
 
 //q 7
-function multiChoice(){
-  const multArray =["Games", "FFXIV", "Tinder", "Star Wars books", "Board Games", "Trolling"];
-  for ( let i = 0; i < 6; i++){
+function multiChoice() {
+  const multArray = [
+    "Games",
+    "FFXIV",
+    "Tinder",
+    "Star Wars books",
+    "Board Games",
+    "Trolling",
+  ];
+  for (let i = 0; i < 6; i++) {
+    var multArrayQuest = prompt(
+      "Welcome to the B-B-B-Bounus Round!! Guess a honny of mine!"
+    );
+    var correctQuest = multArrayQuest.toLowerCase();
 
-    var multArrayQuest = prompt("Welcome to the B-B-B-Bounus Round!! Guess a honny of mine!");
-    var correctQuest =  multArrayQuest.toLowerCase()
-    
-    for ( let i = 0; i < multArray.length; i++){
-      if ( correctQuest == multArray[i].toLowerCase()){
-        score++
-        alert("You got it " + userName + "! You've completed the Guessing Game! Good Job");
-        return; 
+    for (let i = 0; i < multArray.length; i++) {
+      if (correctQuest == multArray[i].toLowerCase()) {
+        score++;
+        alert(
+          "You got it " +
+            userName +
+            "! You've completed the Guessing Game! Good Job"
+        );
+        return;
       }
     }
-    alert("Nope, nope, nope. Try again")
+    alert("Nope, nope, nope. Try again");
   }
 
-  alert("Almost there. Try it out " + userName + "!" + multArray)
+  alert("Almost there. Try it out " + userName + "!" + multArray);
 }
+multiChoice();
+
+alert(
+  "Thanks for playing " +
+    userName +
+    "! Your final score is  " +
+    score +
+    " out of 7"
+);
+
+debugger;
